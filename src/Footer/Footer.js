7d2d107402bf33f01392b505b566e6d6237443eb
Michael Halverson <michaelhalverson12@gmail.com>
2020-10-12 23:54:46 -0400

    Brought Personal Budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer class="bottom" aria-label="footer of page">
+        <div class="center" aria-label="footer of page">
+            All rights reserved &copy; Fabio Nolasco
+            <a href="https://validator.w3.org/">html-validator</a>
+            <a href="https://jigsaw.w3.org/css-validator/">css-validator</a>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Hero() {
+  return (
+    <header className="hero" aria-label="header of page">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+        
+    </header>
+  );
+}
+
+export default Hero;
